c00176076fd6e4f50ddf334145d37fbf130f2cf6
AsdWELL <dimagodov.23@mail.ru>
2026-03-08 20:38:00 +0300

    Добавьте файлы проекта.

---
+++ InternshipPlatform.Domain/Entities/StudentProfiles.cs
@@ -0,0 +1,29 @@
+﻿namespace InternshipPlatform.Domain.Entities
+{
+    public class StudentProfiles
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTime BirthdayDate { get; set; }
+
+        public string Phone { get; set; }
+
+        public string VkLink { get; set; }
+
+        public string TgLink { get; set; }
+
+        public string GithubLink { get; set; }
+
+        public string University { get; set; }
+
+        public string Specialization { get; set; }
+
+        public int GraduationYear { get; set; }
+    }
+}
---
+++ InternshipPlatform.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace InternshipPlatform.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public bool IsVerified { get; set; }
+
+        public DateTime LastLogin { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
